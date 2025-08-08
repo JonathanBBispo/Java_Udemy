2e041b382769c5cc0757b00701d59942586321cc
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-07 22:37:58 -0300

    learn(ap-12): encerra aula de enumeracao

---
+++ EnumComposicao/Aul01.java
@@ -0,0 +1,20 @@
+package EnumComposicao;
+
+import EnumComposicao.Entities.Order;
+import EnumComposicao.Entities.Enums.OrderStatus;
+
+import java.util.Date;
+
+public class Aul01 {
+    public static void main(String[] args) {
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED"); //conversão de string para enum
+
+        System.out.println(os1);
+        System.out.println(os2);
+    }
+}
