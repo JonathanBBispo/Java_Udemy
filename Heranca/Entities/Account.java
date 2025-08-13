051e8f7bfedfda3f72daa91e51abf2f0046c8219
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-12 21:27:34 -0300

    learn(ap-13): encerra aula de heranca

---
+++ Heranca/Entities/Account.java
@@ -0,0 +1,43 @@
+package Heranca.Entities;
+
+public class Account {
+    private int number;
+    private String holder;
+    protected double balance;
+
+    public Account(){}
+    public Account(int number, String holder, double balance){
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
