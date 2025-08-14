fd6621f2392b563f170a4913dfbcf0158b2ca86a
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-14 20:51:20 -0300

    learn(ap-13): encerra aula de sobreposicao e super

--- Heranca/Entities/SavingsAccount.java
+++ Heranca/Entities/SavingsAccount.java
@@ -16,6 +16,11 @@ public class SavingsAccount extends Account{
         balance += balance * interestRate;
     }
 
+    @Override
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
     public double getInterestRate() {
         return interestRate;
     }
