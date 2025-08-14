3159495b00dd755cbd1eaaef1543e06a3783ea32
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-14 20:52:20 -0300

    learn(ap-13): encerra aula de sobreposicao e super

--- Heranca/Entities/Account.java
+++ Heranca/Entities/Account.java
@@ -13,7 +13,7 @@ public class Account {
     }
 
     public void withdraw(double amount){
-        balance -= amount;
+        balance -= amount + 5.0;
     }
 
     public void deposit(double amount){
