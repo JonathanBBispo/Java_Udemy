9554e0fb6aabe68b86711f1dd447ff4df226f7ad
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-23 01:26:20 -0300

    learn(ap-14): cria excecao bussiness exceptions do ex1

---
+++ Excecoes/Exceptions/BussinessException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+public class BussinessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BussinessException(String msg){
+      super(msg);
+    }
+}
